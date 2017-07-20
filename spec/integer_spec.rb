541c0535b033faee0f8d983dbf7dfa85e48d7155
haazime <h4zime@gmail.com>
2017-07-20 21:54:49 +0900

    cleanup

--- spec/integer_spec.rb
+++ spec/integer_spec.rb
@@ -3,42 +3,6 @@ require 'spec_helper'
 RSpec.describe Customer do
   after { Customer.destroy_all }
 
-  describe 'String <=> CustomType' do
-    let(:person_name) { PersonName.new('Hazime', 'Sueyoshi') }
-
-    it 'should return custom type object' do
-      obj = Customer.new(name: person_name)
-      expect(obj.name).to eq(person_name)
-    end
-
-    it 'should return custom type object from DB' do
-      Customer.create!(name: person_name)
-      db = Customer.last
-      expect(db.name).to eq(person_name)
-    end
-
-    it 'should find by custom type attribute' do
-      Customer.create!(name: person_name)
-      expect(Customer.where(name: person_name)).to_not be_nil
-    end
-
-    it 'should NOT find by custom type attribute' do
-      other_person_name = PersonName.new('Other', 'Person')
-      Customer.create!(name: other_person_name)
-      expect(Customer.where(name: person_name)).to be_empty
-    end
-
-    it 'should validate custom type attribute' do
-      aggregate_failures do
-        obj = Customer.new
-        expect(obj.valid?(:name)).to be_falsey
-
-        obj.name = person_name
-        expect(obj.valid?(:name)).to be_truthy
-      end
-    end
-  end
-
   describe 'Integer <=> CustomType' do
     let(:height) { Height.new(183) }
 
